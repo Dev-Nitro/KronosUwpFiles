7dcefc4849c294dcdfe555670dc0f8690758e67d
Dev-Nitro <66803517+Dev-Nitro@users.noreply.github.com>
2023-07-21 18:32:03 -0400

    Create Load.lua

---
+++ Monaco/Load.lua
@@ -0,0 +1,10 @@
+--[[
+
+Thank you for using Kronos
+Make sure you inject before executing a script
+This is where you can paste your scripts
+Join our discord if you have any questions https://discord.gg/bwYwNrKpwJ
+Have fun exploiting
+Made by Nitro
+
+]]--
